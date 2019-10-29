212056ee8f6dafd698e369bb1e53195476ec5147
Julierme Veiga <julierme.puc@gmail.com>
2019-10-28 23:05:44 -0300

    Teste primeira classe de dominio - Categoria

--- src/main/java/com/juliermeveiga/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/juliermeveiga/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.juliermeveiga.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.juliermeveiga.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "rest está ok";
+	public List<Categoria> listar() {
+		Categoria c = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<Categoria>();
+		list.add(c);
+		list.add(c2);
+		
+		return list;
 	}
 }
